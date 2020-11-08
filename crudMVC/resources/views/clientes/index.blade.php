1659fd3c6c9c27219b121b73f12649246f4437aa
pc casa <carlos123df@gmail.com>
2020-11-07 17:38:02 -0700

    search barch V2

--- crudMVC/resources/views/clientes/index.blade.php
+++ crudMVC/resources/views/clientes/index.blade.php
@@ -13,6 +13,20 @@
     
     <x-search-bar></x-search-bar>
     <br>
+    @if ($clientes->count() == 0)
+    <br>
+    <div class="bg-yellow-100 border-t-4 border-yellow-500 rounded-b text-yellow-900 px-4 py-3 shadow-md" role="alert">
+        <div class="flex">
+            <div class="py-1"><svg class="fill-current h-6 w-6 text-yellow-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+            <div>
+                <p class="font-bold">Opss..</p>
+                <p class="text-sm">No se encontraron resultados</p>
+            </div>
+        </div>
+    </div>
+    <br>
+         
+    @else
 
     <table class="bg-white rounded-lg shadow overflow-hidden max-w-2xl mx-auto">
         <thead class="bg-gray-100 border-b border-gray-300">
@@ -27,30 +41,41 @@
             </tr>
 
         <tbody>
+
+        
+        
         @foreach ($clientes as $cliente)
-            <tr class="text-center">
-                <td class="px-6 py-4">{{ $cliente->rfc}}</td>
-                <td class="px-6 py-4">{{ $cliente->nombre}}</td>
-                <td class="px-6 py-4">{{ $cliente->edad}}</td>
-                <td class="px-6 py-4">{{ $cliente->idCiudad}}</td>
-                <td class="px-6 py-4">
-                    <div class="inline-flex">
-                        <a href="{{route('clientes.edit', $cliente)}}" class=" bg-blue-300 hover:bg-blue-400 text-blue-800 font-bold py-1 px-2 rounded-l">
-                          Editar
-                        </a>
-                        
-                        <form class="form" action="{{route('clientes.destroy', $cliente)}}" method="POST" >
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class=" bg-red-300 hover:bg-red-400 text-red-800 font-bold py-1 px-2 rounded-r">
-                            Borrar
-                            </button>
-                                                  
-                        </form>
-                      </div>
-                </td>
-            </tr>
+        <tr class="text-center">
+            <td class="px-6 py-4">{{ $cliente->rfc}}</td>
+            <td class="px-6 py-4">{{ $cliente->nombre}}</td>
+            <td class="px-6 py-4">{{ $cliente->edad}}</td>
+            <td class="px-6 py-4">{{ $cliente->idCiudad}}</td>
+            <td class="px-6 py-4">
+                <div class="inline-flex">
+                    <a href="{{route('clientes.edit', $cliente)}}" class=" bg-blue-300 hover:bg-blue-400 text-blue-800 font-bold py-1 px-2 rounded-l">
+                      Editar
+                    </a>
+                    
+                    <form class="form" action="{{route('clientes.destroy', $cliente)}}" method="POST" >
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class=" bg-red-300 hover:bg-red-400 text-red-800 font-bold py-1 px-2 rounded-r">
+                        Borrar
+                        </button>
+                                              
+                    </form>
+                  </div>
+            </td>
+        </tr>
+        
+        
+        
         @endforeach
+        @endif
+        
+
+      
+        
         </tbody>
         </thead>
     </table>
